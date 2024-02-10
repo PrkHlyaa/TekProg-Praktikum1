0f9fa9dc3b25df0db66effeea9caf354f82ccefd
PrkHlyaa <150895570+PrkHlyaa@users.noreply.github.com>
2024-02-10 20:40:10 +0700

    Add files via upload

---
+++ src/Soal1.java
@@ -0,0 +1,13 @@
+
+public class Soal1 {
+
+	public static void main(String[] args) {
+		byte angka1 = 1;
+		byte angka2 = 6;
+		byte hasil = (byte) (angka1+angka2);
+		
+		System.out.println("Hasil = " +hasil);
+
+	}
+
+}
---
+++ src/Soal2.java
@@ -0,0 +1,11 @@
+
+public class Soal2 {
+
+	public static void main(String[] args) {
+		int i = 50;
+		String s = (i<40)?"life":(i<50)?"universe":"everything";
+		System.out.println(s);
+
+	}
+
+}
